a7931644f8adc85d8a5cac7737d9a9ecf4bc0643
Ripster47 <mpolinski12@gmail.com>
2019-01-08 16:59:06 -0600

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+
+#Welcome to D&D miniatures emporium!
+
+mini_1 = { name: "Rock Troll", size: "large", paint: "painted" }
+mini_2 = { name: "Water Elemental", size: "gargantuan", paint: "painted" }
+mini_3 = { :name => "Kobold High Priest", :size => "small", :paint => "unpainted" }
+
+
+
+puts "Our #{mini_1[:name]} miniature is a #{mini_1[:size]} sized creature and comes totally #{mini_1[:paint]}! "
