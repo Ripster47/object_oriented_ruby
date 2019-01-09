6bbf25a332627ac5f6d777055c5fc918f0b057ec
Ripster47 <mpolinski12@gmail.com>
2019-01-09 10:16:47 -0600

    Store items as class

--- store_item.rb
+++ store_item.rb
@@ -1,10 +1,33 @@
 
 #Welcome to D&D miniatures emporium!
 
-mini_1 = { name: "Rock Troll", size: "large", paint: "painted" }
-mini_2 = { name: "Water Elemental", size: "gargantuan", paint: "painted" }
-mini_3 = { :name => "Kobold High Priest", :size => "small", :paint => "unpainted" }
+# mini_1 = { name: "Rock Troll", size: "large", paint: "painted" }
+# mini_2 = { name: "Water Elemental", size: "gargantuan", paint: "painted" }
+# mini_3 = { :name => "Kobold High Priest", :size => "small", :paint => "unpainted" }
 
 
 
-puts "Our #{mini_1[:name]} miniature is a #{mini_1[:size]} sized creature and comes totally #{mini_1[:paint]}! "
+# puts "Our #{mini_1[:name]} miniature is a #{mini_1[:size]} sized creature and comes totally #{mini_1[:paint]}! "
+
+class Store_items
+
+  attr_reader :name, :size, :paint
+  attr_writer :paint
+
+  def initialize(input_name, input_size, input_paint)
+    @name = input_name
+    @size = input_size
+    @paint = input_paint
+  end
+
+end
+
+mini_1 = Store_items.new("Rock Troll", "Large", "painted")
+mini_2 = Store_items.new("Water elemental", "Gargantuan", "painted")
+mini_3 = Store_items.new("Kobold High Priest", "Small", "unpainted")
+
+p mini_1.name
+p mini_2.size
+p mini_3.paint
+mini_3.paint = "painted"
+p mini_3.paint
